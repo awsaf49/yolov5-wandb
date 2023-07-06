87d13f4352ad2c91a85b3ec715a717181190871d
Awsaf <awsaf49@gmail.com>
2023-07-07 00:19:26 +0600

    fix: identy issue
    *** Please tell me who you are.
    
    Run
    
      git config --global user.email "you@example.com"
      git config --global user.name "Your Name"
    
    to set your account's default identity.
    Omit --global to set the identity only in this repository.
    
    fatal: unable to auto-detect email address (got 'root@07734532f8ad.(none)')

--- add_wandb.sh
+++ add_wandb.sh
@@ -12,9 +12,11 @@ git clone https://github.com/awsaf49/yolov5-wandb.git /tmp/yolov5-wandb
 cd $original_repo
 
 # Merge wandb in yolov5
+git config user.email "you@example.com"
+git config user.name "Your Name"
 git remote add yolov5-wandb /tmp/yolov5-wandb
 git fetch yolov5-wandb
-git merge yolov5-wandb/master
+git merge yolov5-wandb/master -m "add: wandb"
 git remote remove yolov5-wandb
 rm -rf /tmp/yolov5-wandb
 echo "WandB is integrated successfully in YOLOv5"
